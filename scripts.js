5a4df984094a45080416ac54309b66af3fe2032b
Martin <monymartin1703@gmail.com>
2025-02-18 02:53:00 +0300

    Update scripts.js2

--- scripts.js
+++ scripts.js
@@ -269,7 +269,7 @@ button.forEach((button) => {
             toggleState(button);
             setTimeout(() => {
                   page.focus();
-            }, 70);
+            }, 60);
       });
 });
 
